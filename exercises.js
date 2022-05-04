a310416e7010b8ba8f08dbae6cdda34075f54af2
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-04 07:51:19 -0400

    exercises homework

--- exercises.js
+++ exercises.js
@@ -1,27 +1,48 @@
-(function() {
+(function () {
   'use strict';
 
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
 
-
+  function max(num1, num2) {
+    let largerNum;
+    if (num1 > num2) {
+      largerNum = num1;
+    } else {
+      largerNum = num2;
+    }
+    return largerNum;
+  }
+  console.log(max(5, 10));
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
 
-
+  function maxOfThree(num1, num2, num3) {
+    console.log(Math.max(1, 2, 3));
+  }
+  maxOfThree();
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
-
+  const vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  function isVowel(letter1) {
+    console.log(letter1);
+    if (letter1 === vowel)
+      return = "true";
+    else
+      return = "false";
+  }
+  isVowel('a')
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
+
 
 
 
@@ -35,7 +56,13 @@
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
   // ---------------------
 
-
+  function reverse(forwardSentence) {
+    let backwardSentence = ""
+    for (let i = forwardSentence.length; i > -1; i--) {
+      backwardSentence += forwardSentence.charAt(i);
+    }
+    return backwardSentence;
+  }
 
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
@@ -47,46 +74,66 @@
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   // ---------------------
 
-
+  function filterLongWords(arr, i) {
+    let shortenedArray = [];
+    for (let x = 0; x < arr.length; x++) {
+      if (arr[x].length > i) {
+        shortenedArray.push(arr[x])
+      }
+    }
+    return shortenedArray;
+  }
 
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
   // ---------------------
 
+  function charFreq(str) {
+    let totals = {};
+    for (let i = 0; i < str.length; i++) {
+      let character = str[i];
+      if (totals.hasOwnProperty(character)) {
+        totals[character] = totals[character] + 1;
+      } else {
+        totals[character] = 1;
+      }
+    }
+    return totals;
 
-  ////////////////////////////////////////////////////////////////////////
-  /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
-  ////////////////////////////////////////////////////////////////////////
 
-  console.assert(max(2, 4) === 4, 'function max error');
+    ////////////////////////////////////////////////////////////////////////
+    /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
+    ////////////////////////////////////////////////////////////////////////
 
-  console.assert(maxOfThree(1, 2, 3) === 3, 'ERROR funtion maxOfThree');
+    console.assert(max(2, 4) === 4, 'function max error');
 
-  console.assert(maxOfThree(2, 3, 1) === 3, 'ERROR funtion maxOfThree');
+    console.assert(maxOfThree(1, 2, 3) === 3, 'ERROR funtion maxOfThree');
 
-  console.assert(maxOfThree(3, 2, 1) === 3, 'ERROR maxOfThree');
+    console.assert(maxOfThree(2, 3, 1) === 3, 'ERROR funtion maxOfThree');
 
-  console.assert(isVowel('a') === true, 'ERROR function isVowel');
+    console.assert(maxOfThree(3, 2, 1) === 3, 'ERROR maxOfThree');
 
-  console.assert(isVowel('b') == false, 'ERROR function isVowel');
+    console.assert(isVowel('a') === true, 'ERROR function isVowel');
 
-  console.assert(rovarspraket("this is fun") === "tothohisos isos fofunon", 'ERROR function rovarspraket');
+    console.assert(isVowel('b') == false, 'ERROR function isVowel');
 
-  console.assert(sum([1, 2, 3, 4]) === 10, 'ERROR function sum');
+    console.assert(rovarspraket("this is fun") === "tothohisos isos fofunon", 'ERROR function rovarspraket');
 
-  console.assert(multiply([1, 2, 3, 4]) === 24, 'ERROR function multiply');
+    console.assert(sum([1, 2, 3, 4]) === 10, 'ERROR function sum');
 
-  console.assert(reverse('jag testar') === 'ratset gaj', 'ERROR function reverse');
+    console.assert(multiply([1, 2, 3, 4]) === 24, 'ERROR function multiply');
 
-  console.assert(findLongestWord(['hello', 'world', 'does','anyone', 'really', 'know', 'what', 'time', 'it', 'is']) === 6, 'ERROR function findLongestWord');
+    console.assert(reverse('jag testar') === 'ratset gaj', 'ERROR function reverse');
 
-  console.assert(filterLongWords(['hello', 'world', 'does','anyone', 'really', 'know', 'what', 'time', 'it', 'is'], 4).length === 4, 'ERROR function filterLongWords');
+    console.assert(findLongestWord(['hello', 'world', 'does', 'anyone', 'really', 'know', 'what', 'time', 'it', 'is']) === 6, 'ERROR function findLongestWord');
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').a === 7);
+    console.assert(filterLongWords(['hello', 'world', 'does', 'anyone', 'really', 'know', 'what', 'time', 'it', 'is'], 4).length === 4, 'ERROR function filterLongWords');
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').b === 14);
+    console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').a === 7);
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').c === 3);
+    console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').b === 14);
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').d === 3);
-})();
+    console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').c === 3);
+
+    console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').d === 3);
+  }) ();
